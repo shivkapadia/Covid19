03eb7e1c84cddab417b2034c33033b75745b4e75
Shiv Kapadia <shivkapadia6@gmail.com>
2024-06-13 02:08:53 -0400

    a

--- model.py
+++ model.py
@@ -5,7 +5,7 @@ from tensorflow.keras.regularizers import l2
 
 def build_model(input_shape, num_classes):
     model = Sequential([
-    Conv2D(32, (3, 3), activation='relu', input_shape=(256, 256, 2)),
+    Conv2D(32, (3, 3), activation='relu', input_shape=(256, 256, 1)),
     MaxPooling2D((2, 2)),
     Conv2D(64, (3, 3), activation='relu'),
     MaxPooling2D((2, 2)),
